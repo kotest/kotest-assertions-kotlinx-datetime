d9ddf832bf25d8d3d58a9b49b0f837921ccdc803
sksamuel <sam@sksamuel.com>
2021-03-08 20:17:19 -0600

    Project migrated from main kotest repo

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,17 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.31"
+   const val org = "io.kotest.extensions"
+
+   object Kotest {
+      private const val version = "4.4.3"
+      const val assertionsShared = "io.kotest:kotest-assertions-shared:$version"
+      const val api = "io.kotest:kotest-framework-api:$version"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+   }
+
+   object KotlinTime {
+      private const val version = "0.1.1"
+      const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:$version"
+   }
+}
